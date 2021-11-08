7bf47ccbee3234652ed38f36deb08ac1f33bf7f4
Anton <ushkov6@mail.ru>
2021-11-08 18:59:35 +0400

    item details part 1

--- src/components/itemDetails/itemDetails.js
+++ src/components/itemDetails/itemDetails.js
@@ -1,56 +1,55 @@
-import React, {Component} from 'react';
+import {useState, useEffect} from 'react';
+
+import React from 'react';
 import './itemDetails.css';
 
-class ItemDetails extends Component {
+const ItemDetails = (props) => {
 
+    const [item, setItem] = useState(null);
+    const [itemId, getData] = useState(props);
+    const [prevProps] = useState(props);
+    const [data, setData] = useState(props);
 
-    state = {
-        item: null
-    }
+    useEffect(() => {
+        updateItem();
+    }, []);
 
-    componentDidMount() {
-        this.updateItem();
-    }
-    
-    componentDidUpdate(prevProps) {
-        if (this.props.itemId !== prevProps.itemId) {
-            this.updateItem();
+    useEffect(() => {
+        if (itemId !== prevProps.itemId) {
+            updateItem();
         }
-    }
+    });
 
-    updateItem() {
-        const {itemId, getData} = this.props;
+    function updateItem() {
         if (!itemId) {
             return;
         }
 
         getData(itemId)
-            .then((item) => {
-                this.setState({item})
+            .then((data) => {
+                setItem({data});
             })
     }
 
-    render() {
 
-        if (!this.state.item) {
-            return <span className='select-error'>Please select item in the list</span>
-        }
-        const {item} = this.state;
-        const {name} = item;
-
-        return (
-            <div className="char-details rounded">
-                <h4>{name}</h4>
-                <ul className="list-group list-group-flush">
-                    {
-                        React.Children.map(this.props.children, (child) => {
-                            return React.cloneElement(child, {item})
-                        })
-                    }
-                </ul>
-            </div>
-        );
+    if (!item) {
+        return <span className='select-error'>Please select item in the list</span>
     }
+
+    const {name} = item;
+
+    return (
+        <div className="char-details rounded">
+            <h4>{name}</h4>
+            <ul className="list-group list-group-flush">
+                {
+                    React.Children.map(this.props.children, (child) => {
+                        return React.cloneElement(child, {item})
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default ItemDetails;
